33e6de43c024f5d00f83082e05db22768829534e
mdekauwe <mdekauwe@gmail.com>
2020-04-16 13:30:03 +1000

    Added new water potentials plot

---
+++ other_western_sites_cable_hydraulics/plot_water_potentials_3_sites.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+
+"""
+Plot SWP
+
+That's all folks.
+"""
+__author__ = "Martin De Kauwe"
+__version__ = "1.0 (18.10.2017)"
+__email__ = "mdekauwe@gmail.com"
+
+import netCDF4 as nc
+import matplotlib.pyplot as plt
+import sys
+import datetime as dt
+import pandas as pd
+import numpy as np
+from matplotlib.ticker import FixedLocator
+import datetime
+import os
+import glob
+import string
+
+def main(fname1, fname2, fname3, met_fname1, met_fname2, met_fname3, odir,
+         labelsx):
+    SEC_2_HLFHOUR = 1800.
+
+    df1 = read_cable_file(fname1, type="CABLE")
+    df2 = read_cable_file(fname2, type="CABLE")
+    df3 = read_cable_file(fname3, type="CABLE")
+
+    df1_met = read_cable_file(met_fname1, type="MET")
+    df2_met = read_cable_file(met_fname2, type="MET")
+    df3_met = read_cable_file(met_fname3, type="MET")
+
+    print(fname1, met_fname1)
+    print(fname2, met_fname2)
+    print(fname3, met_fname3)
+
+
+    df1_met['Rainf'] *= SEC_2_HLFHOUR
+    df2_met['Rainf'] *= SEC_2_HLFHOUR
+    df3_met['Rainf'] *= SEC_2_HLFHOUR
+    method = {'Rainf':'sum'}
+    df1_met = df1_met.resample("D").agg(method)
+    df2_met = df2_met.resample("D").agg(method)
+    df3_met = df3_met.resample("D").agg(method)
+
+    fig = plt.figure(figsize=(9,12))
+    fig.subplots_adjust(hspace=0.05)
+    fig.subplots_adjust(wspace=0.2)
+    plt.rcParams['text.usetex'] = False
+    plt.rcParams['font.family'] = "sans-serif"
+    plt.rcParams['font.sans-serif'] = "Helvetica"
+    plt.rcParams['axes.labelsize'] = 14
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['legend.fontsize'] = 14
+    plt.rcParams['xtick.labelsize'] = 14
+    plt.rcParams['ytick.labelsize'] = 14
+    colours = plt.cm.Set2(np.linspace(0, 1, 7))
+
+    labels_gen = label_generator('lower', start="(", end=")")
+    props = dict(boxstyle='round', facecolor='white', alpha=1.0,
+                 ec="white")
+
+    ax1 = fig.add_subplot(3,1,1)
+    ax2 = fig.add_subplot(3,1,2)
+    ax3 = fig.add_subplot(3,1,3)
+
+    axx1 = ax1.twinx()
+    axx2 = ax2.twinx()
+    axx3 = ax3.twinx()
+
+    df1x = df1[(df1.index.hour == 6)].copy()
+    df1y = df1[(df1.index.hour == 12)].copy()
+
+    df2x = df2[(df2.index.hour == 6)].copy()
+    df2y = df2[(df2.index.hour == 12)].copy()
+
+    df3x = df3[(df3.index.hour == 6)].copy()
+    df3y = df3[(df3.index.hour == 12)].copy()
+
+    axes = [ax1,ax2,ax3]
+    #axes2 = [axx1, ax]
+
+
+    ax1.plot(df1.index, df1["weighted_psi_soil"], c=colours[0],
+             lw=1.5, ls="-", label="$\Psi$$_{sw}$", zorder=2)
+    ax1.plot(df1x.index, df1x["psi_stem"], c=colours[1],
+             lw=1.5, ls="-", label="$\Psi$$_{x}$", zorder=1)
+    ax1.plot(df1y.index, df1y["psi_leaf"], c=colours[2],
+             lw=1.5, ls="-", label="$\Psi$$_{l}$")
+
+
+    ax2.plot(df2.index, df2["weighted_psi_soil"], c=colours[0],
+             lw=1.5, ls="-", label="$\Psi$$_{sw}$", zorder=2)
+    ax2.plot(df2x.index, df2x["psi_stem"], c=colours[1],
+             lw=1.5, ls="-", label="$\Psi$$_{x}$", zorder=1)
+    ax2.plot(df2y.index, df2y["psi_leaf"], c=colours[2],
+             lw=1.5, ls="-", label="$\Psi$$_{l}$")
+
+
+    ax3.plot(df3.index, df3["weighted_psi_soil"], c=colours[0],
+             lw=1.5, ls="-", label="$\Psi$$_{sw}$", zorder=2)
+    ax3.plot(df3x.index, df3x["psi_stem"], c=colours[1],
+             lw=1.5, ls="-", label="$\Psi$$_{x}$", zorder=1)
+    ax3.plot(df3y.index, df3y["psi_leaf"], c=colours[2],
+             lw=1.5, ls="-", label="$\Psi$$_{l}$")
+
+    ax1.set_ylim(-5, 0.1)
+    ax2.set_ylim(-5, 0.1)
+    ax3.set_ylim(-5, 0.1)
+
+    axx1.plot(df1_met.index, df1_met["Rainf"].cumsum(), alpha=0.3,
+              color="black", label="Rainfall")
+    axx2.plot(df2_met.index, df2_met["Rainf"].cumsum(), alpha=0.3, color="black")
+    axx3.plot(df3_met.index, df3_met["Rainf"].cumsum(), alpha=0.3, color="black")
+
+
+    print(df1_met["Rainf"].cumsum()[-1])
+    print(df2_met["Rainf"].cumsum()[-1])
+    print(df3_met["Rainf"].cumsum()[-1])
+    axx1.set_ylim(0, 3000)
+    axx2.set_ylim(-5, 3000)
+    axx3.set_ylim(-5, 3000)
+
+    #axx1.bar(df1_met.index, df1_met["Rainf"], alpha=0.3, color="black")
+    #axx1.set_yticks([0, 15, 30])
+
+    #axx2.bar(df2_met.index, df2_met["Rainf"], alpha=0.3, color="black")
+    #axx2.set_yticks([0, 15, 30])
+
+    #axx3.bar(df3_met.index, df3_met["Rainf"], alpha=0.3, color="black")
+    #axx3.set_yticks([0, 15, 30])
+
+    ax2.set_ylabel("Water potential (MPa)")
+    axx2.set_ylabel("Cumulative Rainfall (mm)")
+
+    ax1.legend(numpoints=1, loc=(0.01, 0.14), frameon=False)
+    axx1.legend(numpoints=1, loc=(0.01, 0.44), frameon=False)
+
+    for a in axes:
+        a.set_xlim([datetime.date(2016,1,1), datetime.date(2020, 1, 1)])
+
+    from matplotlib.ticker import MaxNLocator
+    ax1.yaxis.set_major_locator(MaxNLocator(5))
+    ax2.yaxis.set_major_locator(MaxNLocator(5))
+    ax3.yaxis.set_major_locator(MaxNLocator(5))
+    axx1.yaxis.set_major_locator(MaxNLocator(4))
+    axx2.yaxis.set_major_locator(MaxNLocator(4))
+    axx3.yaxis.set_major_locator(MaxNLocator(4))
+
+    plt.setp(ax1.get_xticklabels(), visible=False)
+    plt.setp(ax2.get_xticklabels(), visible=False)
+
+    fig_label = "%s %s" % (next(labels_gen), labelsx[0])
+    ax1.text(0.7, 0.1, fig_label,
+            transform=ax1.transAxes, fontsize=14, verticalalignment='top',
+            bbox=props)
+    fig_label = "%s %s" % (next(labels_gen), labelsx[1])
+    ax2.text(0.7, 0.1, fig_label,
+            transform=ax2.transAxes, fontsize=14, verticalalignment='top',
+            bbox=props)
+    fig_label = "%s %s" % (next(labels_gen), labelsx[2])
+    ax3.text(0.7, 0.1, fig_label,
+            transform=ax3.transAxes, fontsize=14, verticalalignment='top',
+            bbox=props)
+
+    plot_fname = os.path.join(odir, "three_western_sites_water_potentials.pdf")
+    fig.savefig(plot_fname, bbox_inches='tight', pad_inches=0.1)
+
+def read_cable_file(fname, type=None):
+
+    f = nc.Dataset(fname)
+    time = nc.num2date(f.variables['time'][:],
+                        f.variables['time'].units)
+    #print (f.variables['time'][0], time[0], time[1])
+
+    if type == "CABLE":
+        df = pd.DataFrame(f.variables['weighted_psi_soil'][:,0,0],
+                          columns=['weighted_psi_soil'])
+        df['psi_leaf'] = f.variables['psi_leaf'][:,0,0]
+        df['psi_stem'] = f.variables['psi_stem'][:,0,0]
+        df['psi_soil1'] = f.variables['psi_soil'][:,0,0,0]
+        df['psi_soil2'] = f.variables['psi_soil'][:,1,0,0]
+        df['psi_soil3'] = f.variables['psi_soil'][:,2,0,0]
+        df['psi_soil4'] = f.variables['psi_soil'][:,3,0,0]
+        df['psi_soil5'] = f.variables['psi_soil'][:,4,0,0]
+        df['psi_soil6'] = f.variables['psi_soil'][:,5,0,0]
+        df['LAI'] = f.variables['LAI'][:,0,0]
+        print(df.LAI.max())
+    elif type == "MET":
+
+        df = pd.DataFrame(f.variables['Rainf'][:,0,0], columns=['Rainf'])
+
+    df['dates'] = time
+    df = df.set_index('dates')
+
+    return df
+
+def label_generator(case='lower', start='', end=''):
+    choose_type = {'lower': string.ascii_lowercase,
+                   'upper': string.ascii_uppercase}
+    generator = ('%s%s%s' %(start, letter, end) for letter in choose_type[case])
+
+    return generator
+
+if __name__ == "__main__":
+
+
+    fname1 = "outputs/hydraulics_-30.40_151.60.nc"
+    met_fname1 = "AWAP_single_pixel_-30.40_151.60.nc"
+    fname2 = "outputs/hydraulics_-31.10_150.95.nc"
+    met_fname2 = "AWAP_single_pixel_-31.10_150.95.nc"
+    fname3 = "outputs/hydraulics_-31.10_142.50.nc"
+    met_fname3 = "AWAP_single_pixel_-31.10_142.50.nc"
+
+    labelsx = ["30.40$^{\circ}$S,151.60$^{\circ}$E", \
+               "31.10$^{\circ}$S,150.95$^{\circ}$E", \
+               "31.10$^{\circ}$S,142.50$^{\circ}$E"]
+    odir = "/Users/mdekauwe/Dropbox/Drought_risk_paper/figures/figs"
+    #odir = "/Users/mdekauwe/Desktop"
+    main(fname1, fname2, fname3, met_fname1, met_fname2, met_fname3, odir,
+         labelsx)
---
+++ other_western_sites_cable_hydraulics/run_cable_site.py
@@ -0,0 +1,279 @@
+#!/usr/bin/env python
+
+"""
+Run CABLE either for a single site, a subset, or all the flux sites pointed to
+in the met directory
+
+- Only intended for biophysics
+- Set mpi = True if doing a number of flux sites
+
+That's all folks.
+"""
+__author__ = "Martin De Kauwe"
+__version__ = "1.0 (02.08.2018)"
+__email__ = "mdekauwe@gmail.com"
+
+import os
+import sys
+import glob
+import shutil
+import subprocess
+import multiprocessing as mp
+import numpy as np
+
+from cable_utils import adjust_nml_file
+from cable_utils import get_svn_info
+from cable_utils import change_LAI
+from cable_utils import add_attributes_to_output_file
+from cable_utils import change_iveg
+
+class RunCable(object):
+
+    def __init__(self, met_dir=None, log_dir=None, output_dir=None,
+                 restart_dir=None, aux_dir=None, namelist_dir=None,
+                 nml_fname="cable.nml",
+                 veg_fname="def_veg_params_zr_clitt_albedo_fix.txt",
+                 soil_fname="def_soil_params.txt",
+                 grid_fname="gridinfo_AWAP_OpenLandMap_new_iveg.nc",
+                 phen_fname="modis_phenology_csiro.txt",
+                 cnpbiome_fname="pftlookup_csiro_v16_17tiles.csv",
+                 elev_fname="GSWP3_gwmodel_parameters.nc", fwsoil="standard",
+                 lai_dir=None, fixed_lai=None, co2_conc=400.0,
+                 met_subset=[], cable_src=None, cable_exe="cable", mpi=True,
+                 num_cores=None, verbose=True):
+
+        self.met_dir = met_dir
+        self.log_dir = log_dir
+        self.output_dir = output_dir
+        self.restart_dir = restart_dir
+        self.aux_dir = aux_dir
+        self.namelist_dir = namelist_dir
+        self.nml_fname = nml_fname
+        self.biogeophys_dir = os.path.join(self.aux_dir, "core/biogeophys")
+        self.grid_dir = os.path.join(self.aux_dir, "offline")
+        self.biogeochem_dir = os.path.join(self.aux_dir, "core/biogeochem/")
+        self.veg_fname = os.path.join(self.biogeophys_dir, veg_fname)
+        self.soil_fname = os.path.join(self.biogeophys_dir, soil_fname)
+        #self.grid_fname = os.path.join(self.grid_dir, grid_fname)
+        self.grid_fname = os.path.join("/Users/mdekauwe/Desktop", grid_fname)
+        self.phen_fname = os.path.join(self.biogeochem_dir, phen_fname)
+        self.cnpbiome_fname = os.path.join(self.biogeochem_dir, cnpbiome_fname)
+        self.elev_fname = elev_fname
+        self.co2_conc = co2_conc
+        self.met_subset = met_subset
+        self.cable_src = cable_src
+        self.cable_exe = os.path.join(cable_src, "offline/%s" % (cable_exe))
+        self.verbose = verbose
+        self.mpi = mpi
+        self.num_cores = num_cores
+        self.lai_dir = lai_dir
+        self.fixed_lai = fixed_lai
+        self.fwsoil = fwsoil
+
+
+    def main(self):
+
+        (met_files, url, rev) = self.initialise_stuff()
+
+        # Setup multi-processor jobs
+        if self.mpi:
+            if self.num_cores is None: # use them all!
+                self.num_cores = mp.cpu_count()
+            chunk_size = int(np.ceil(len(met_files) / float(self.num_cores)))
+            pool = mp.Pool(processes=self.num_cores)
+            processes = []
+
+            for i in range(self.num_cores):
+                start = chunk_size * i
+                end = chunk_size * (i + 1)
+                if end > len(met_files):
+                    end = len(met_files)
+
+                # setup a list of processes that we want to run
+                p = mp.Process(target=self.worker,
+                               args=(met_files[start:end], url, rev, ))
+                processes.append(p)
+
+            # Run processes
+            for p in processes:
+                p.start()
+        else:
+            self.worker(met_files, url, rev)
+
+    def worker(self, met_files, url, rev):
+
+        for fname in met_files:
+
+            site = "AWAP_pixel"
+            lat = os.path.basename(fname).split("_")[3]
+            lon1 = os.path.basename(fname).split("_")[4].split(".")[0]
+            lon2 = os.path.basename(fname).split("_")[4].split(".")[1]
+            lon = "%s.%s" % (lon1, lon2)
+            lat = float(lat)
+            lon = float(lon)
+
+            base_nml_fn = os.path.join(self.grid_dir, "%s" % (self.nml_fname))
+            nml_fname = "cable_%s.nml" % (site)
+            shutil.copy(base_nml_fn, nml_fname)
+
+            (out_fname, out_log_fname) = self.clean_up_old_files(site)
+
+            # Add LAI to met file?
+            if self.fixed_lai is not None or self.lai_dir is not None:
+                fname = change_LAI(fname, site, fixed=self.fixed_lai,
+                                   lai_dir=self.lai_dir)
+
+            if self.fwsoil == "hydraulics":
+                if lat == -31.10 and lon == 142.50:
+                    fname = change_iveg(fname, site, 21) # GRW
+                elif lat == -31.10 and lon == 150.95:
+                    fname = change_iveg(fname, site, 22) # SAW
+            elif self.fwsoil == "standard":
+                fname = change_iveg(fname, site, 2)
+
+            replace_dict = {
+                            "filename%met": "'%s'" % (fname),
+                            "filename%out": "'%s'" % (out_fname),
+                            "filename%log": "'%s'" % (out_log_fname),
+                            "filename%restart_out": "' '",
+                            "filename%type": "'%s'" % (self.grid_fname),
+                            "filename%veg": "'%s'" % (self.veg_fname),
+                            "filename%soil": "'%s'" % (self.soil_fname),
+                            "output%restart": ".FALSE.",
+                            "fixedCO2": "%.2f" % (self.co2_conc),
+                            "casafile%phen": "'%s'" % (self.phen_fname),
+                            "casafile%cnpbiome": "'%s'" % (self.cnpbiome_fname),
+                            "cable_user%GS_SWITCH": "'medlyn'",
+                            "cable_user%GW_MODEL": ".FALSE.",
+                            "cable_user%or_evap": ".FALSE.",
+                            "redistrb": ".FALSE.",
+                            "spinup": ".FALSE.",
+                            "cable_user%litter": ".TRUE.",
+                            "cable_user%FWSOIL_SWITCH": "'%s'" % (self.fwsoil),
+            }
+            adjust_nml_file(nml_fname, replace_dict)
+
+            self.run_me(nml_fname)
+
+            add_attributes_to_output_file(nml_fname, out_fname, url, rev)
+            shutil.move(nml_fname, os.path.join(self.namelist_dir, nml_fname))
+
+            if self.fixed_lai is not None or self.lai_dir is not None:
+                os.remove("%s_tmp.nc" % (site))
+
+            os.remove("new_sumbal")
+
+    def initialise_stuff(self):
+
+        if not os.path.exists(self.restart_dir):
+            os.makedirs(self.restart_dir)
+
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
+
+        if not os.path.exists(self.log_dir):
+            os.makedirs(self.log_dir)
+
+        if not os.path.exists(self.namelist_dir):
+            os.makedirs(self.namelist_dir)
+
+        # Run all the met files in the directory
+        if len(self.met_subset) == 0:
+            met_files = glob.glob(os.path.join(self.met_dir, "*.nc"))
+        else:
+            met_files = [os.path.join(self.met_dir, i) for i in self.met_subset]
+
+        cwd = os.getcwd()
+        (url, rev) = get_svn_info(cwd, self.cable_src)
+
+        # delete local executable, copy a local copy and use that
+        local_exe = "cable"
+        if os.path.isfile(local_exe):
+            os.remove(local_exe)
+        shutil.copy(self.cable_exe, local_exe)
+        self.cable_exe = local_exe
+
+        return (met_files, url, rev)
+
+    def clean_up_old_files(self, site):
+        out_fname = os.path.join(self.output_dir, "%s_out.nc" % (site))
+        if os.path.isfile(out_fname):
+            os.remove(out_fname)
+
+        out_log_fname = os.path.join(self.log_dir, "%s_log.txt" % (site))
+        if os.path.isfile(out_log_fname):
+            os.remove(out_log_fname)
+
+        return (out_fname, out_log_fname)
+
+    def run_me(self, nml_fname):
+        # run the model
+        if self.verbose:
+            cmd = './%s %s' % (self.cable_exe, nml_fname)
+            error = subprocess.call(cmd, shell=True)
+            if error is 1:
+                print("Job failed to submit")
+                raise
+        else:
+            # No outputs to the screen: stout and stderr to dev/null
+            cmd = './%s %s > /dev/null 2>&1' % (self.cable_exe, nml_fname)
+            error = subprocess.call(cmd, shell=True)
+            if error is 1:
+                print("Job failed to submit")
+
+if __name__ == "__main__":
+
+    #------------- Change stuff ------------- #
+    #met_dir = "../../met_data/plumber_met"
+    met_dir = "./"
+    #met_dir = os.getcwd()
+    log_dir = "logs"
+    output_dir = "outputs"
+    restart_dir = "restart_files"
+    namelist_dir = "namelists"
+
+    cable_src = "/Users/mdekauwe/research/CABLE_runs/src/trunk_DESICA_PFTs/trunk_DESICA_PFTs/"
+    aux_dir = "/Users/mdekauwe/research/CABLE_runs/src/CABLE-AUX/"
+    mpi = False
+    num_cores = 4 # set to a number, if None it will use all cores...!
+    # if empty...run all the files in the met_dir
+    #met_subset = ['TumbarumbaOzFlux2.0_met.nc']
+    #met_subset = ['WombatStateForestOzFlux2.0_met.nc']
+    #met_subset = ['GreatWesternWoodlandsOzFlux2.0_met.nc']
+    # ------------------------------------------- #
+    met_subset = ['AWAP_single_pixel_-30.40_151.60.nc']
+    C = RunCable(met_dir=met_dir, log_dir=log_dir, output_dir=output_dir,
+                 restart_dir=restart_dir, aux_dir=aux_dir,
+                 namelist_dir=namelist_dir, met_subset=met_subset,
+                 cable_src=cable_src, mpi=mpi, num_cores=num_cores,
+                 fwsoil="hydraulics")
+    C.main()
+
+    out_fname = os.path.join(output_dir, "AWAP_pixel_out.nc")
+    shutil.move(out_fname, os.path.join(output_dir, "hydraulics_-30.40_151.60.nc"))
+
+
+    """
+    met_subset = ['AWAP_single_pixel_-31.10_142.50.nc']
+    C = RunCable(met_dir=met_dir, log_dir=log_dir, output_dir=output_dir,
+                 restart_dir=restart_dir, aux_dir=aux_dir,
+                 namelist_dir=namelist_dir, met_subset=met_subset,
+                 cable_src=cable_src, mpi=mpi, num_cores=num_cores,
+                 fwsoil="hydraulics")
+    C.main()
+
+    out_fname = os.path.join(output_dir, "AWAP_pixel_out.nc")
+    shutil.move(out_fname, os.path.join(output_dir, "hydraulics_-31.10_142.50.nc"))
+    
+
+    met_subset = ['AWAP_single_pixel_-31.10_150.95.nc']
+    C = RunCable(met_dir=met_dir, log_dir=log_dir, output_dir=output_dir,
+                 restart_dir=restart_dir, aux_dir=aux_dir,
+                 namelist_dir=namelist_dir, met_subset=met_subset,
+                 cable_src=cable_src, mpi=mpi, num_cores=num_cores,
+                 fwsoil="hydraulics")
+    C.main()
+
+    out_fname = os.path.join(output_dir, "AWAP_pixel_out.nc")
+    shutil.move(out_fname, os.path.join(output_dir, "hydraulics_-31.10_150.95.nc"))
+    """
